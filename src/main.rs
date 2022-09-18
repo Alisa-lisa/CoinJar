d0251049b13be869aefe5486633f48b3c26ab7e5
alisa <alisa.dammer@gmail.com>
2022-09-18 12:53:40 +0200

    wrap in rocket

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use anyhow::Result;
 use std::fs::OpenOptions;
 use std::fs::{File};
 use std::io::{Write, Read, Seek};
-use std::io::{Error, ErrorKind};
+use std::sync::Mutex;
+use rocket::routes;
+use rocket::get;
+use rocket::State;
+
 // const fn - !SPECIAL! function that can be computed during compile time 
 
 fn increase(file: &mut File) -> Result<u64>{
     // read out the value
     let mut buffer = [0; 8];
     // check if file even contains any info
+    file.rewind()?;
     file.read_exact(&mut buffer).unwrap_or_default();
     // cast bytes + increase
     let mut res = u64::from_be_bytes(buffer);
@@ -20,12 +25,25 @@ fn increase(file: &mut File) -> Result<u64>{
     Ok(res)
 }
 
-fn main() -> Result<()> {
-    let mut file = OpenOptions::new().write(true).read(true).create(true).open("coins.txt")?;
-    let current_coins = increase(&mut file);
+#[get("/")]
+fn index(file: &State<Mutex<File>>) -> String {
+    let mut prot = file.lock().unwrap();
+    let current_coins = increase(&mut prot).unwrap();
+    format!("Currently {current_coins:?} coins in the jar")
+}
+
+#[rocket::main]
+async fn main() -> Result<()> {
+    let file = OpenOptions::new().write(true).read(true).create(true).open("coins.txt")?;
     // try to set coins to 0 if no jar was yet created
     // None is not Option<T> in this implementation, I can not just write None
     // increase
-    println!("Currently {current_coins:?} coins in the jar");
+    let protected = Mutex::new(file);
+    let _rocket = rocket::build()
+        .manage(protected)
+        .mount("/hello", routes![index])
+        .launch()
+        .await?;
+
     Ok(())
 }
